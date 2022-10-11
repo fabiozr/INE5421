57b340b3dbc9ff371da90183e5e4f5777ca4cd17
Fabio Pereira dos Santos <fpereira@pop-os.localdomain>
2022-10-11 13:23:52 -0300

    muda: parser de classe para funções estaticas

--- parser.py
+++ parser.py
@@ -1,67 +1,76 @@
-# Classe responsavel por fazer o parser inicial de uma expressão regular, para transformala em uma arvore sintatica.
-class RegexParser:
-    operatorsPrecedence = {
+""" Modulo responsavel por fazer agrupar as funções que fazem o parser de um regex"""
+
+
+def parseRegex(regex: str):
+    """ Recebe uma string representando um regex e retorna uma string em um formato mais facil de transformar em uma arvore sintatica"""
+    extendedRegex = extendsExpression(regex)
+    procesedRegx = addConcatenationSymbol(extendedRegex)
+    postfixRegex = toPostfix(procesedRegx)
+    return postfixRegex
+
+
+def extendsExpression(expr: str):
+    """ Adiciona o simbolo de concatenação "#" que representa uma expressão regular extendida """
+    return f"{expr}#"
+
+
+def addConcatenationSymbol(expr: str):
+    """ Adiciona o simbolo de concatenação "." explicitamente em uma expressão regular """
+    output = ""
+    for i in range(len(expr)):
+        token = expr[i]
+        output += token
+
+        if token in ["(", "|"]:
+            continue
+
+        if i < (len(expr) - 1):
+
+            lookahead = expr[i + 1]
+            if lookahead in [")", "|", "*"]:  # ? + [ ] -
+                continue
+
+            output += "."
+
+    return output
+
+
+def getOperatorsPrecedence():
+    """Retorna um dicionario com a precedencia dos operadores utilizados no regex"""
+    return {
         "(": -1,
         "|": 0,
         ".": 1,
         "*": 2,
     }
 
-    def __init__(self, expr):
-        self.expr = expr
 
-    # Adiciona o simbolo de concatenação "." explicitamente em uma expressão regular
-    def __preProcess(self):
-        output = ""
-        for i in range(len(self.expr)):
-            token = self.expr[i]
-            output += token
+def toPostfix(expr: str):
+    """Transforma uma expressão regular infixa para posfixa"""
+    output = ""
 
-            if token in ["(", "|"]:
-                continue
+    stack = []
+    stack.append("(")
+    extendedExpr = expr + ')'
+    operatorsPrecedence = getOperatorsPrecedence()
 
-            if i < (len(self.expr) - 1):
+    for i in extendedExpr:
+        if i == "(":
+            stack.append(i)
+        elif i == ")":
+            while stack[-1] != "(":
+                output += stack.pop()
+            stack.pop()
+        elif i in dict.keys(operatorsPrecedence):
+            while len(stack) > 0:
+                charPrecedence = operatorsPrecedence[i]
+                topStackPrecedence = operatorsPrecedence[stack[-1]]
 
-                lookahead = self.expr[i + 1]
-                if lookahead in [")", "|", "*"]:  # ? + [ ] -
-                    continue
-
-                output += "."
-
-        return output
-
-    # Transforma uma expressão regular infixa para posfixa
-    def __toPostfix(self):
-        output = ""
-
-        stack = []
-        stack.append("(")
-        extendedExpr = self.expr + ')'
-
-        for i in extendedExpr:
-            if i == "(":
-                stack.append(i)
-            elif i == ")":
-                while stack[-1] != "(":
+                if charPrecedence > topStackPrecedence:
+                    stack.append(i)
+                    break
+                else:
                     output += stack.pop()
-                stack.pop()
-            elif i in dict.keys(self.operatorsPrecedence):
-                while len(stack) > 0:
-                    charPrecedence = self.operatorsPrecedence[i]
-                    topStackPrecedence = self.operatorsPrecedence[stack[-1]]
-
-                    if charPrecedence > topStackPrecedence:
-                        stack.append(i)
-                        break
-                    else:
-                        output += stack.pop()
-            else:
-                output += i
-        return output
-
-
-parser = RegexParser()
-parsed = parser.preProcess(regex)
-postfix = parser.toPostfix(parsed)
-
-print(postfix)
+        else:
+            output += i
+    return output
